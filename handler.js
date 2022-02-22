189ed0e5b3eb9fc7b7de825a626957489f1c7561
venkatakireeti <Venkata@getluckyvr.com>
2022-02-22 12:48:02 -0500

    Severless Flag Player

--- handler.js
+++ handler.js
@@ -23,7 +23,7 @@ module.exports.run = async (event, context) => {
     .then(result => console.log(`Status code: ${result}`))
     .catch(err => console.error(`Error doing the request for the event: ${JSON.stringify(event)} => ${err}`));
   // Dev Table to dev api
-  await doPostRequestDev(JSON.stringify(now_dev.rows),"fact_slot_players_flagged_dev")
+  await doPostRequestDev(JSON.stringify(now_dev.rows), "fact_slot_players_flagged_dev")
     .then(result => console.log(`Status code dev: ${result}`))
     .catch(err => console.error(`Error doing the request for the event: ${JSON.stringify(event)} => ${err}`));
   // Main Table to dev api
@@ -113,3 +113,48 @@ const doPostRequestDev = (playersFlagged, table_name = "fact_slot_players_flagge
     req.end();
   });
 };
+
+
+module.exports.slotRun = async (event, context) => {
+  const pool = new Pool({
+    user: 'admin',
+    host: 'redshift-cluster-1.cn4rvtbrptq4.us-east-2.redshift.amazonaws.com',
+    database: 'dev',
+    password: 'LuckyRedshift777!',
+    port: 5439,
+  });
+  const RTP_MAX = 2;
+  const START_DATE = new Date();
+  const END_DATE = new Date(new Date().setMinutes(new Date().getMinutes() - 30));
+  //const END_DATE = new Date(new Date().setHours(new Date().getHours() - 1));
+
+
+  // Query that looks at stage live 
+  const now = await pool.query(`select * from(select PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY rtp) as rtp, COUNT(user_name) as spin, machine_id,user_name,user_id from (select (CAST(total AS FLOAT)/CAST(totalbet AS FLOAT)) as rtp, machine_id, user_name,user_id from (select (totalwinnings+jackpotwins) as total, totalbet, machine_id,user_name,user_id from (select machine_id,user_id ,user_name,( case when win_amount is null then 0 else win_amount end) as JackpotWins,winnings as totalwinnings,( case when was_free_game = true then 0 else bet end) as TotalBet from "dev"."public"."fact_slot_machine_spin" join "dev"."public"."dim_user" as dim_user on user_name=SUBSTRING(REPLACE(spin_id,'.','_'),1,CHARINDEX('-',spin_id)-1) full outer join "dev"."public"."fact_slot_jackpot" as jackpot  on "dev"."public"."fact_slot_machine_spin".spin_id = jackpot.spin_id where "dev"."public"."fact_slot_machine_spin".time >  ${END_DATE.getTime()} AND "dev"."public"."fact_slot_machine_spin".time < ${START_DATE.getTime()} AND stage = 'live' AND in_tournament_mode = false )) where total != 0 AND totalbet != 0  ) group by user_name,machine_id,user_id ORDER BY user_name) where rtp > 2`);
+
+  //TODO - new query that looks at stage preview
+  const dev_now = await pool.query(`select * from(select PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY rtp) as rtp, COUNT(user_name) as spin, machine_id,user_name,user_id from (select (CAST(total AS FLOAT)/CAST(totalbet AS FLOAT)) as rtp, machine_id, user_name,user_id from (select (totalwinnings+jackpotwins) as total, totalbet, machine_id,user_name,user_id from (select machine_id,user_id ,user_name,( case when win_amount is null then 0 else win_amount end) as JackpotWins,winnings as totalwinnings,( case when was_free_game = true then 0 else bet end) as TotalBet from "dev"."public"."fact_slot_machine_spin" join "dev"."public"."dim_user" as dim_user on user_name=SUBSTRING(REPLACE(spin_id,'.','_'),1,CHARINDEX('-',spin_id)-1) full outer join "dev"."public"."fact_slot_jackpot" as jackpot  on "dev"."public"."fact_slot_machine_spin".spin_id = jackpot.spin_id where "dev"."public"."fact_slot_machine_spin".time >  ${END_DATE.getTime()} AND "dev"."public"."fact_slot_machine_spin".time < ${START_DATE.getTime()} AND stage = 'preview' AND in_tournament_mode = false )) where total != 0 AND totalbet != 0  ) group by user_name,machine_id,user_id ORDER BY user_name) where rtp > 2`);
+
+  // Push rtp update into fact_slot_machine_rtp
+  // TODO: Include stage when pushing data to slot_machine_rtp
+  let insertIntoLive = 'INSERT INTO "dev"."public"."fact_slot_players_flagged" (username,date_of_flag,slot_machine_id,median_rtp,number_of_spins,user_id) VALUES (\'' + now.rows[0].user_name + '\',' + (new Date()).getTime() + ',\'' + now.rows[0].machine_id + '\',' + now.rows[0].rtp + ',' + now.rows[0].spin + ',\'' + now.rows[0].user_id + '\')'
+
+  for (let i = 1; i < now.rows.length; i++) {
+    insertIntoLive += ',(\'' + now.rows[i].user_name + '\',' + (new Date()).getTime() + ',\'' + now.rows[i].machine_id + '\',' + now.rows[i].rtp + ',' + now.rows[i].spin + ',\'' + now.rows[i].user_id + '\')';
+
+  }
+
+  await pool.query(insertIntoLive);
+
+  // Change machine status flase in dev
+  if (dev_now.rows.length > 0) {
+    let insertIntoDevQuery = 'INSERT INTO "dev"."public"."fact_slot_players_flagged_dev" (username,date_of_flag,slot_machine_id,median_rtp,number_of_spins,user_id) VALUES (\'' + now.rows[0].user_name + '\',' + (new Date()).getTime() + ',\'' + now.rows[0].machine_id + '\',' + now.rows[0].rtp + ',' + now.rows[0].spin + ',\'' + now.rows[0].user_id + '\')';
+    for (let i = 1; i < dev_now.rows.length; i++) {
+      insertIntoDevQuery += ',(\'' + now.rows[i].user_name + '\',' + (new Date()).getTime() + ',\'' + now.rows[i].machine_id + '\',' + now.rows[i].rtp + ',' + now.rows[i].spin + ',\'' + now.rows[i].user_id + '\')';
+
+    }
+    await pool.query(insertIntoDevQuery);
+  }
+
+  await pool.end();
+}
